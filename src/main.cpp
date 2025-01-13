3e8f90dba090b8f6bd6c71ef552640d3b5f26255
Giang Pham <crystallizedrain0@gmail.com>
2025-01-13 19:15:20 +1000

    cmake test build to verify that SFML 3.0 works

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML 3.0 works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
