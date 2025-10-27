64b1c9ecb785d539e15573848a33e03e51fbd0ff
Giang Pham <crystallizedrain0@gmail.com>
2025-10-27 14:27:43 +1000

    Add class to hold information about a midi device.

---
+++ src/midi/MidiDevice.cpp
@@ -0,0 +1,22 @@
+#include "MidiDevice.hpp"
+
+#include <utility>
+
+namespace raindrops
+{
+    MidiDevice::MidiDevice(std::string portName, const unsigned int portNumber)
+    : m_portName(std::move(portName))
+    , m_portNumber(portNumber)
+    {
+    }
+
+    std::string MidiDevice::getPortName()
+    {
+        return m_portName;
+    }
+
+    unsigned int MidiDevice::getPortNumber() const
+    {
+        return m_portNumber;
+    }
+}
---
+++ src/midi/MidiDevice.hpp
@@ -0,0 +1,26 @@
+#ifndef RAINDROPS_MIDIDEVICE_HPP
+#define RAINDROPS_MIDIDEVICE_HPP
+
+#include "rtmidi/RtMidi.h"
+
+namespace raindrops
+{
+    /**
+     * @brief Holds information about a midi device.
+     */
+    class MidiDevice {
+    public:
+        MidiDevice(std::string portName, unsigned int portNumber);
+
+        std::string getPortName();
+
+        [[nodiscard]] unsigned int getPortNumber() const;
+
+    private:
+        std::string m_portName;
+        unsigned int m_portNumber;
+    };
+}
+
+
+#endif //RAINDROPS_MIDIDEVICE_HPP
