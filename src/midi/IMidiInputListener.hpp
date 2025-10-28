f80ffa261b183d9ae4757d33ff32fa50edfa9acf
Giang Pham <crystallizedrain0@gmail.com>
2025-10-28 15:01:48 +1000

    Add an interface to help with listening and parsing midi messages.

---
+++ src/midi/IMidiInputListener.hpp
@@ -0,0 +1,41 @@
+#ifndef RAINDROPS_IMIDIINPUTLISTENER_HPP
+#define RAINDROPS_IMIDIINPUTLISTENER_HPP
+
+#include <vector>
+
+namespace raindrops
+{
+    /**
+     * @brief Midi listener that can be derived from any class that wants to
+     * have midi event callbacks.
+     */
+    class IMidiInputListener {
+    public:
+        virtual ~IMidiInputListener() = default;
+
+        /**
+         * @brief Called on new midi Note On (9x) event.
+         * @param keyNumber Number of pressed note. There are 128 possible note numbers.
+         * @param velocity How hard/fast a key was pressed. Value between 0 and 127.
+         * @param midiChannel Channel of sent event. There are 16 possible channels.
+         */
+        virtual void onNoteOn(int keyNumber, int velocity, int midiChannel);
+
+        /**
+         * @brief Called on a Note Off (8x) event.
+         * @param keyNumber Number of released note. There are 128 possible note numbers.
+         * @param velocity "Release" velocity of note. Value between 0 and 127.
+         * @param midiChannel Channel of sent event. There are 16 possible channels.
+         */
+        virtual void onNoteOff(int keyNumber, int velocity, int midiChannel);
+
+        /**
+         * @brief Called when a midi event cannot be identified as a Note On or Off.
+         * @param bytes The message of the midi event.
+         * @see https://en.wikipedia.org/wiki/MIDI for more details.
+         */
+        virtual void onMidiEvent(std::vector<unsigned char> bytes) = 0;
+    };
+}
+
+#endif //RAINDROPS_IMIDIINPUTLISTENER_HPP
---
+++ src/midi/MidiMessageList.hpp
@@ -0,0 +1,31 @@
+#ifndef RAINDROPS_MIDIMESSAGELIST_HPP
+#define RAINDROPS_MIDIMESSAGELIST_HPP
+
+/**
+ * @brief Contains several defines used when parsing midi input messages.
+ * https://midi.org/expanded-midi-1-0-messages-list
+ */
+
+#define NOTE_ON             144
+#define NOTE_OFF            128
+#define NOTE_MASK           240
+#define CHANNEL_MASK        15
+#define MIDI_CHANNEL_1      1
+#define MIDI_CHANNEL_2      2
+#define MIDI_CHANNEL_3      3
+#define MIDI_CHANNEL_4      4
+#define MIDI_CHANNEL_5      5
+#define MIDI_CHANNEL_6      6
+#define MIDI_CHANNEL_7      7
+#define MIDI_CHANNEL_8      8
+#define MIDI_CHANNEL_9      9
+#define MIDI_CHANNEL_10     10
+#define MIDI_CHANNEL_11     11
+#define MIDI_CHANNEL_12     12
+#define MIDI_CHANNEL_13     13
+#define MIDI_CHANNEL_14     14
+#define MIDI_CHANNEL_15     15
+#define MIDI_CHANNEL_16     16
+#define MIDI_CHANNEL_ALL    (-1)
+
+#endif //RAINDROPS_MIDIMESSAGELIST_HPP
