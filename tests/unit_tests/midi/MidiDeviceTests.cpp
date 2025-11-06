c385770fdaee89c47cea3db839565540b7e60c66
Giang Pham <crystallizedrain0@gmail.com>
2025-11-06 15:28:51 +1000

    Add tests for MidiDevice class.

---
+++ tests/unit_tests/midi/MidiDeviceTests.cpp
@@ -0,0 +1,20 @@
+#include "../../../src/midi/MidiDevice.hpp"
+#include "doctest/doctest.h"
+
+TEST_SUITE_BEGIN("MidiDevice");
+
+TEST_CASE("getPortName - should return expected name")
+{
+    raindrops::MidiDevice midiDevice { "foo", 1 };
+    std::string expectedPortName { "foo" };
+    CHECK(expectedPortName == midiDevice.getPortName());
+}
+
+TEST_CASE("getPortNumber - should return expected number")
+{
+    raindrops::MidiDevice midiDevice { "foo", 1 };
+    int expectedPortNumber { 1 };
+    CHECK(expectedPortNumber == midiDevice.getPortNumber());
+}
+
+TEST_SUITE_END();
