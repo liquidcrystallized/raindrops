57166c0d0605242fa01aa50789e2f7375bfdfaa7
Giang Pham <crystallizedrain0@gmail.com>
2025-11-07 12:52:35 +1000

    Add tests for MidiReceiver class.

---
+++ tests/unit_tests/midi/MidiReceiverTests.cpp
@@ -0,0 +1,18 @@
+#include "../../../src/midi/MidiReceiver.hpp"
+#include "doctest/doctest.h"
+#include <memory>
+
+TEST_SUITE_BEGIN("MidiReceiver");
+
+TEST_CASE("On key press - should all return true")
+{
+    std::unique_ptr<raindrops::MidiReceiver> midiReceiver { new raindrops::MidiReceiver };
+    unsigned int keyNumber { 60 };
+    unsigned int velocity { 60 };
+    unsigned int midiChannel { 1 };
+
+    CHECK(midiReceiver->onNoteOn(keyNumber, velocity, midiChannel));
+    CHECK(midiReceiver->onNoteOff(keyNumber, velocity, midiChannel));
+}
+
+TEST_SUITE_END();
