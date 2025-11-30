9a8a6df022deeb767438fd7c2a1b251d108b69ed
Giang Pham <crystallizedrain0@gmail.com>
2025-11-30 18:22:07 +1000

    update: Tests for MidiMonitor now include get/set for the channel number and port number.

--- tests/unit_tests/midi/MidiMonitorTests.cpp
+++ tests/unit_tests/midi/MidiMonitorTests.cpp
@@ -10,4 +10,20 @@ TEST_CASE("MidiMonitor starts correctly")
     CHECK(isRunning == true);
 }
 
+TEST_CASE("MidiMonitor sets correct port number")
+{
+    std::unique_ptr<raindrops::MidiMonitor> midiMonitor { new raindrops::MidiMonitor };
+    unsigned int expectedPortNumber { 3 };
+    midiMonitor->setMidiPort(3);
+    CHECK(expectedPortNumber == midiMonitor->getMidiPortNumber());
+}
+
+TEST_CASE("MidiMonitor sets correct channel number")
+{
+    std::unique_ptr<raindrops::MidiMonitor> midiMonitor { new raindrops::MidiMonitor };
+    unsigned int expectedChannelNumber { 3 };
+    midiMonitor->setMidiChannel(3);
+    CHECK(expectedChannelNumber == midiMonitor->getMidiChannelNumber());
+}
+
 TEST_SUITE_END();
