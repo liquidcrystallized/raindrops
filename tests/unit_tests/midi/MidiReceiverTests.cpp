141d4e5198bb9c049ee8affcd1ddff92e6f6103b
Giang Pham <crystallizedrain0@gmail.com>
2025-12-06 13:13:15 +1000

    Test MidiReceiver with more comprehensive cases.

--- tests/unit_tests/midi/MidiReceiverTests.cpp
+++ tests/unit_tests/midi/MidiReceiverTests.cpp
@@ -15,4 +15,23 @@ TEST_CASE("On key press - should all return true")
     CHECK(midiReceiver->onNoteOff(keyNumber, velocity, midiChannel));
 }
 
+TEST_CASE("MidiReceiver handles multiple simultaneous notes")
+{
+    std::unique_ptr<raindrops::MidiReceiver> midiReceiver { new raindrops::MidiReceiver };
+    unsigned int key1 { 60 }, key2 { 61 }, key3 { 62 };
+    unsigned int velocity { 60 };
+    unsigned int midiChannel { 1 };
+
+    // Press multiple keys
+    CHECK(midiReceiver->onNoteOn(key1, velocity, midiChannel));
+    CHECK(midiReceiver->onNoteOn(key2, velocity, midiChannel));
+    CHECK(midiReceiver->onNoteOn(key3, velocity, midiChannel));
+
+    // Release all keys
+    CHECK(midiReceiver->onNoteOff(key1, velocity, midiChannel));
+    CHECK(midiReceiver->onNoteOff(key2, velocity, midiChannel));
+    CHECK(midiReceiver->onNoteOff(key3, velocity, midiChannel));
+}
+
+
 TEST_SUITE_END();
