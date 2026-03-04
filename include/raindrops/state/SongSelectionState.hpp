1d4e5a5bff985a8c1423f34751abe7f6de5b0faa
Giang Pham <crystallizedrain0@gmail.com>
2026-03-05 09:55:03 +1000

    Add basic menu to SongSelectionState alongside routing to PlayingState.

--- include/raindrops/state/SongSelectionState.hpp
+++ include/raindrops/state/SongSelectionState.hpp
@@ -2,7 +2,10 @@
 #define RAINDROPS_SONGSELECTIONSTATE_HPP
 
 #include "State.hpp"
+#include "VerticalStackPanel.hpp"
+#include <raygui-cpp/Button.h>
 #include <raygui-cpp/Label.h>
+#include <raygui-cpp/ListView.h>
 
 namespace raindrops
 {
@@ -18,12 +21,33 @@ namespace raindrops
 
         void onWindowResize() override;
     private:
+        VerticalStackPanel m_verticalStackPanel;
+        raylib::Rectangle m_verticalStackPanelSize;
+        rgc::Bounds m_verticalStackPanelBounds;
+
+        rgc::ListView m_songListView;
+        rgc::Bounds m_songListViewBounds;
+        int m_songListViewScrollIndex;
+        int m_songListViewActiveSelection;
+        const char* m_songListViewText;
+
+        rgc::Button m_playButton;
+        rgc::Button m_backButton;
+        rgc::Button m_refreshButton;
+        raylib::Rectangle m_buttonSize;
+
+        const char* m_playButtonText;
+        const char* m_backButtonText;
+        const char* m_refreshButtonText;
+        int m_buttonScalingFactor;
+
         std::vector<MidiDevice> m_midiDevices;
         rgc::Label m_connectedDeviceLabel;
         std::string m_deviceList;
         std::string m_connectedDeviceLabelText;
 
         void positionUIComponents();
+        void refreshSongList();
     };
 }
 
--- src/state/SongSelectionState.cpp
+++ src/state/SongSelectionState.cpp
@@ -1,11 +1,22 @@
 #include "SongSelectionState.hpp"
+#include "PlayingState.hpp"
 #include "StateMachine.hpp"
+#include <raygui-cpp/Utils.h>
 
 namespace raindrops
 {
     SongSelectionState::SongSelectionState(StateMachine& stateMachine, Renderer& renderer, MidiMonitor& midiMonitor, const bool replace)
     : State { stateMachine, renderer, midiMonitor, replace, "SongSelectionState" }
     {
+        m_songListViewText = "Lorem;Ipsum;Dolor;Sit;Amet;Consectetuer";
+        m_songListViewScrollIndex = 1;
+        m_songListViewActiveSelection = 0;
+
+        m_playButtonText = "Play";
+        m_backButtonText = "Back";
+        m_refreshButtonText = "Refresh";
+
+        refreshSongList();
         positionUIComponents();
     }
 
@@ -28,12 +39,19 @@ namespace raindrops
 
         m_connectedDeviceLabelText = "Connected device: " + m_midiMonitor.getConnectedDeviceName();
         m_connectedDeviceLabel.SetText(m_connectedDeviceLabelText.c_str());
+
+        m_verticalStackPanel.Update();
+        m_songListView.Update();
     }
 
     void SongSelectionState::draw()
     {
         m_renderer.drawStart();
+
+        RAYGUI_CPP_UNUSED(m_verticalStackPanel.Show(true));
+        RAYGUI_CPP_UNUSED(m_songListView.Show(true));
         RAYGUI_CPP_UNUSED(m_connectedDeviceLabel.Show(true));
+
         m_renderer.drawEnd();
     }
 
@@ -45,10 +63,104 @@ namespace raindrops
     //TODO: Temp programmers UI, implement actual design later, throw it into the renderer
     void SongSelectionState::positionUIComponents()
     {
+        m_buttonScalingFactor = 1;
+
+        // Sets the bounds/workspace/position limit for other controls.
+        // They should all be rendered inside the bounds of the stack panel.
+        m_verticalStackPanelSize  = raylib::Rectangle {
+            0,
+            0,
+            static_cast<float>(m_renderer.getWindowWidth()) / 2.0f,
+            static_cast<float>(m_renderer.getWindowHeight()) / 1.5f
+        };
+
+        const Vector2 verticalStackPanelPosition {
+            this->m_windowCentrePosition.getX() - m_verticalStackPanelSize.GetWidth() / 2,
+            this->m_windowCentrePosition.getY() - m_verticalStackPanelSize.GetHeight() / 2
+        };
+
+        m_verticalStackPanel = VerticalStackPanel(
+            rgc::Bounds(
+                {
+                    verticalStackPanelPosition.getX(),
+                    verticalStackPanelPosition.getY()
+                },
+                {
+                    m_verticalStackPanelSize.GetWidth(),
+                    m_verticalStackPanelSize.GetHeight()
+                }
+            ),
+            "song selection (temporary look/ui)"
+        );
+
+        const Vector2 songListViewSize {
+            m_verticalStackPanelSize.GetWidth() / 1.1f,
+            m_verticalStackPanelSize.GetHeight() / 1.5f
+        };
+
+        const Vector2 songListViewPosition {
+            m_verticalStackPanelSize.GetWidth() - songListViewSize.getX(),
+            m_verticalStackPanelSize.GetHeight() - songListViewSize.getY()
+        };
+
+        // List view for all connected midi devices.
+        m_songListViewBounds = rgc::Bounds {
+                {
+                    verticalStackPanelPosition.getX() + (songListViewPosition.getX() / 2),
+                    verticalStackPanelPosition.getY() + (songListViewPosition.getY() / 12)
+                },
+                {
+                    songListViewSize.getX(),
+                    songListViewSize.getY()
+                }
+        };
+        m_songListView = rgc::ListView(m_songListViewBounds, m_songListViewText, &m_songListViewScrollIndex, m_songListViewActiveSelection);
+        m_songListView.SetStyle(rgc::Style(rgc::Style::Position::CENTER, { 0, -m_verticalStackPanelSize.GetHeight() / 10.0f }));
+
+        // Size and position the button relative to the stack panel.
+        m_buttonSize = raylib::Rectangle {
+            0,
+            0,
+            m_verticalStackPanelSize.GetWidth() / 1.1f,
+            m_verticalStackPanelSize.GetHeight() / 8.0f
+        };
+
+        // Back button positioning and callback.
+        m_backButton = rgc::Button(rgc::Bounds::WithText(
+            m_backButtonText,
+            m_buttonScalingFactor,
+            { m_buttonSize.GetWidth() / 4, m_buttonSize.GetHeight() }),
+            m_backButtonText);
+        m_backButton.SetStyle(rgc::Style(rgc::Style::Position::BOTTOM_CENTER, { -m_buttonSize.GetWidth() / 5.0f, -m_verticalStackPanelSize.GetHeight() / 12.0f }));
+        m_backButton.OnClick([this]
+        {
+            m_stateMachine.lastState();
+        });
+        m_verticalStackPanel.AddChild(rgc::ToComponent(&m_backButton));
+
+        // Play button positioning and callback.
+        m_playButton = rgc::Button(rgc::Bounds::WithText(
+            m_playButtonText,
+            m_buttonScalingFactor,
+            { m_buttonSize.GetWidth() / 4, m_buttonSize.GetHeight() }),
+            m_playButtonText);
+        m_playButton.SetStyle(rgc::Style(rgc::Style::Position::BOTTOM_CENTER, { m_buttonSize.GetWidth() / 5.0f, -m_verticalStackPanelSize.GetHeight() / 12.0f }));
+        m_playButton.OnClick([this]
+        {
+            //TODO: Pass loaded song details to play screen for sheet rendering.
+            m_next = StateMachine::build<PlayingState>(m_stateMachine, m_renderer, m_midiMonitor, false);
+        });
+        m_verticalStackPanel.AddChild(rgc::ToComponent(&m_playButton));
+
         // Show currently connected device.
         m_connectedDeviceLabel = rgc::Label{ rgc::Bounds
             { 10, static_cast<float>(m_renderer.getWindowHeight()) - 20.0f, static_cast<float>(m_renderer.getWindowWidth()), 10 },
             m_connectedDeviceLabelText.c_str() };
         m_connectedDeviceLabel.SetStyle(rgc::Style(rgc::Style::Position::CENTER, { 0, 0 }));
+    }
+
+    void SongSelectionState::refreshSongList()
+    {
+        //TODO
     }
 }
