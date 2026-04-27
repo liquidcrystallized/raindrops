7a18809a6c0d7e148a3647db8454c57f08c67c58
Giang Pham <crystallizedrain0@gmail.com>
2026-04-27 13:55:17 +1000

    Add ScoreFactory tests.

---
+++ tests/unit_tests/core/score/ScoreFactoryTests.cpp
@@ -0,0 +1,102 @@
+#include "MxReader.hpp"
+#include "MusicXmlReader.hpp"
+#include "MusicSheet.hpp"
+#include "ScoreFactory.hpp"
+#include "TestInputFiles.hpp"
+#include <doctest.h>
+#include <sstream>
+
+TEST_SUITE_BEGIN("ScoreFactory");
+
+TEST_CASE("ScoreFactory::createFromReader creates valid MusicSheet")
+{
+    raindrops::MxReader mx {};
+    raindrops::MusicXmlReader musicXmlReader { mx };
+
+    musicXmlReader.tryLoadFileIntoStream(clairDeLuneFilePath);
+    std::istringstream inputStream { musicXmlReader.getRawFileContents() };
+    musicXmlReader.tryParseFileInputStream(inputStream);
+
+    auto musicSheet = raindrops::ScoreFactory::createFromReader(musicXmlReader);
+
+    CHECK( musicSheet != nullptr );
+    CHECK( !musicSheet->getTitle().empty() );
+    CHECK( !musicSheet->getComposer().empty() );
+}
+
+TEST_CASE("ScoreFactory sets correct title from MusicXML")
+{
+    raindrops::MxReader mx {};
+    raindrops::MusicXmlReader musicXmlReader { mx };
+
+    musicXmlReader.tryLoadFileIntoStream(clairDeLuneFilePath);
+    std::istringstream inputStream { musicXmlReader.getRawFileContents() };
+    musicXmlReader.tryParseFileInputStream(inputStream);
+
+    auto musicSheet = raindrops::ScoreFactory::createFromReader(musicXmlReader);
+
+    CHECK( musicSheet->getTitle() == musicXmlReader.getSongTitle() );
+}
+
+TEST_CASE("ScoreFactory sets correct composer from MusicXML")
+{
+    raindrops::MxReader mx {};
+    raindrops::MusicXmlReader musicXmlReader { mx };
+
+    musicXmlReader.tryLoadFileIntoStream(clairDeLuneFilePath);
+    std::istringstream inputStream { musicXmlReader.getRawFileContents() };
+    musicXmlReader.tryParseFileInputStream(inputStream);
+
+    auto musicSheet = raindrops::ScoreFactory::createFromReader(musicXmlReader);
+
+    CHECK( musicSheet->getComposer() == musicXmlReader.getSongComposer() );
+}
+
+TEST_CASE("ScoreFactory creates correct number of measures")
+{
+    raindrops::MxReader mx {};
+    raindrops::MusicXmlReader musicXmlReader { mx };
+
+    musicXmlReader.tryLoadFileIntoStream(clairDeLuneFilePath);
+    std::istringstream inputStream { musicXmlReader.getRawFileContents() };
+    musicXmlReader.tryParseFileInputStream(inputStream);
+
+    auto musicSheet = raindrops::ScoreFactory::createFromReader(musicXmlReader);
+
+    CHECK( musicSheet->getMeasureCount() == musicXmlReader.getNumberOfMeasures() );
+}
+
+TEST_CASE("ScoreFactory measures have sequential numbers")
+{
+    raindrops::MxReader mx {};
+    raindrops::MusicXmlReader musicXmlReader { mx };
+
+    musicXmlReader.tryLoadFileIntoStream(clairDeLuneFilePath);
+    std::istringstream inputStream { musicXmlReader.getRawFileContents() };
+    musicXmlReader.tryParseFileInputStream(inputStream);
+
+    auto musicSheet = raindrops::ScoreFactory::createFromReader(musicXmlReader);
+
+    const auto& measures = musicSheet->getMeasures();
+    for (int i = 0; i < measures.size(); i++)
+    {
+        CHECK( measures[i].getMeasureNumber() == i + 1 );
+    }
+}
+
+TEST_CASE("ScoreFactory handles empty measure count")
+{
+    raindrops::MxReader mx {};
+    raindrops::MusicXmlReader musicXmlReader { mx };
+
+    musicXmlReader.tryLoadFileIntoStream(noMeasuresXmlFilePath);
+    std::istringstream inputStream { musicXmlReader.getRawFileContents() };
+    musicXmlReader.tryParseFileInputStream(inputStream);
+
+    auto musicSheet = raindrops::ScoreFactory::createFromReader(musicXmlReader);
+
+    CHECK( musicSheet != nullptr );
+    CHECK( musicSheet->getMeasureCount() == 1 ); // May be an mx thing, no measures will still add 1 empty measure with on data.
+}
+
+TEST_SUITE_END;
