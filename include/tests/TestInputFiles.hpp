57a3e2aac3ff9de8c6b62fd6173b47025a1b7ecb
Giang Pham <crystallizedrain0@gmail.com>
2026-04-27 13:54:18 +1000

    Add musicxml test file with no measures.

--- include/tests/TestInputFiles.hpp
+++ include/tests/TestInputFiles.hpp
@@ -6,6 +6,7 @@
 inline std::string pitchesPitchesFilePath { "../../tests/inputs/musicxml/01a-Pitches-Pitches.xml" };
 inline std::string helloWorldFilePath { "../../tests/inputs/musicxml/hello-world.xml" };
 inline std::string nonMusicXmlFilePath { "../../tests/inputs/musicxml/non-musicxml.xml" };
+inline std::string noMeasuresXmlFilePath { "../../tests/inputs/musicxml/no-measures.xml" };
 
 inline std::string clairDeLuneFilePath { "../../tests/inputs/musicxml/Claude_Debussy-Clair_de_Lune.musicxml" };
 inline std::string airOnTheGStringFilePath { "../../tests/inputs/musicxml/J.S_Bach-Air_on_the_G_String.musicxml" };
