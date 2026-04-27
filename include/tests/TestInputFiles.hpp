005e8b038406820d4b3316d93bb09b8c36f5a8a6
Giang Pham <crystallizedrain0@gmail.com>
2026-04-27 13:36:49 +1000

    Add some public domain musicxml files for testing.

--- include/tests/TestInputFiles.hpp
+++ include/tests/TestInputFiles.hpp
@@ -7,4 +7,7 @@ inline std::string pitchesPitchesFilePath { "../../tests/inputs/musicxml/01a-Pit
 inline std::string helloWorldFilePath { "../../tests/inputs/musicxml/hello-world.xml" };
 inline std::string nonMusicXmlFilePath { "../../tests/inputs/musicxml/non-musicxml.xml" };
 
+inline std::string clairDeLuneFilePath { "../../tests/inputs/musicxml/Claude_Debussy-Clair_de_Lune.musicxml" };
+inline std::string airOnTheGStringFilePath { "../../tests/inputs/musicxml/J.S_Bach-Air_on_the_G_String.musicxml" };
+
 #endif //RAINDROPS_TESTINPUTFILES_HPP
