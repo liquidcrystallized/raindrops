17e4868fb46950b72a78d25eca7a68f7eebece84
Giang Pham <crystallizedrain0@gmail.com>
2026-07-07 17:09:30 +1000

    Application config loading/creation on startup.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include "Application.hpp"
+#include "ConfigManager.hpp"
 #include "GraphicsRaylib.hpp"
 
 int main()
 {
+    auto& configManger = raindrops::ConfigManager::getInstance();
+
+    bool configLoaded = configManger.loadFromDefaultLocation();
+
+    if (!configLoaded)
+    {
+        std::cout << "Creating and using default config." << std::endl;
+        configManger.resetToDefaults();
+        configManger.saveToDefaultLocation();
+        configManger.loadFromDefaultLocation();
+    }
+
     raindrops::GraphicsRaylib renderer {}; //TODO: Choose renderer based on config.
     raindrops::Application application { renderer };
 
