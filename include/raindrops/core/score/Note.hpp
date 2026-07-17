689bebd1e23f2490048eabbf2335ba7eefde4298
Giang Pham <crystallizedrain0@gmail.com>
2026-07-17 14:58:25 +1000

    Add empty Note class.

---
+++ include/raindrops/core/score/Note.hpp
@@ -0,0 +1,12 @@
+#ifndef RAINDROPS_NOTE_HPP
+#define RAINDROPS_NOTE_HPP
+
+
+namespace raindrops
+{
+    class Note {
+    };
+}
+
+
+#endif //RAINDROPS_NOTE_HPP
---
+++ tests/unit_tests/core/score/NoteTests.cpp
@@ -0,0 +1,6 @@
+#include "Note.hpp"
+#include <doctest.h>
+
+TEST_SUITE_BEGIN("Note");
+
+TEST_SUITE_END;
